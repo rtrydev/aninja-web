a753fe5679387e88bbe2c6d6d906f2d5b81fd748
rtrydev <r.kulka44@gmail.com>
2022-03-19 19:45:36 +0100

    add getall getbyid create tests

---
+++ aninja-anime-service-tests/AnimeRepoTest.cs
@@ -0,0 +1,186 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
+using aninja_anime_service.Data;
+using aninja_anime_service.Enums;
+using aninja_anime_service.Models;
+using aninja_anime_service.Repositories;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Moq;
+using Xunit;
+
+namespace aninja_anime_service_tests;
+
+public class AnimeRepoTest
+{
+    //methodName_testedScenario_expectedBehaviour
+    
+    // arrange -> act -> assert
+    
+    private readonly List<Anime> _data = new ()
+    {
+        new ()
+        {
+            Id = 1, OriginalTitle = "あいうえお", TranslatedTitle = "Title1", Status = Status.CurrentlyAiring,
+            Demographic = Demographic.Josei, Description = "test desc1", ImgUrl = null, EndDate = new DateTime(),
+            StartDate = new DateTime(2020, 12, 1), EpisodeCount = 12
+        },
+        new ()
+        {
+            Id = 2, OriginalTitle = "かきくけこ", TranslatedTitle = "Title2", Status = Status.FinishedAiring,
+            Demographic = Demographic.Seinen, Description = "test desc2", ImgUrl = "https://google.com",
+            EndDate = new DateTime(2021, 4, 5), StartDate = new DateTime(2020, 12, 1), EpisodeCount = 10
+        },
+        new ()
+        {
+            Id = 3, OriginalTitle = "たちつてと", TranslatedTitle = "Title3", Status = Status.NotYetAired,
+            Demographic = Demographic.Shoujo, Description = "test desc3", ImgUrl = null, EndDate = new DateTime(),
+            StartDate = new DateTime(2023, 12, 1), EpisodeCount = 0
+        }
+    };
+    
+    [Fact]
+    public async Task GetAll_GetWithoutQuery_ReturnsAll()
+    {
+        //Arrange
+        var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("AnimeRepoTest")
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        await using var context = new AppDbContext(contextOptions);
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+
+        context.AddRange(_data);
+        await context.SaveChangesAsync();
+        
+        var repo = new AnimeRepository(context);
+
+        //Act
+        var anime = await repo.GetAll();
+        
+        //Assert
+        anime.Should().Equal(_data);
+
+    }
+
+    [Fact]
+    public async Task GetById_GetById_ReturnsEntityWithGivenIdOrNull()
+    {
+        //Arrange
+        var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("AnimeRepoTest")
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        await using var context = new AppDbContext(contextOptions);
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+
+        context.AddRange(_data);
+        await context.SaveChangesAsync();
+        
+        var repo = new AnimeRepository(context);
+
+        //Act
+        var anime0 = await repo.GetById(0);
+
+        var animeMinus = await repo.GetById(-1);
+        
+        var anime1 = await repo.GetById(1);
+        var anime2 = await repo.GetById(2);
+        var anime3 = await repo.GetById(3);
+        
+        //Assert
+        anime0.Should().BeNull();
+        animeMinus.Should().BeNull();
+        
+        anime1.Should().BeSameAs(_data[0]);
+        anime2.Should().BeSameAs(_data[1]);
+        anime3.Should().BeSameAs(_data[2]);
+    }
+
+    [Fact]
+    public async Task Create_AddNewEntry_AddsNewEntry()
+    {
+        //Arrange
+        var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("AnimeRepoTest")
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        await using var context = new AppDbContext(contextOptions);
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+
+        context.AddRange(_data);
+        await context.SaveChangesAsync();
+        
+        var repo = new AnimeRepository(context);
+
+        //Act
+        var animeToAdd = new Anime()
+        {
+            Id = 4, OriginalTitle = "さしすせそ", TranslatedTitle = "Some title", Demographic = Demographic.Josei,
+            Description = "Desc", Status = Status.CurrentlyAiring, StartDate = new DateTime(2021, 11, 11),
+            EndDate = new DateTime(), EpisodeCount = 12, ImgUrl = null
+        };
+
+        var result = await repo.Create(animeToAdd);
+        await repo.SaveChangesAsync();
+
+        //Assert
+        var dataAfterAdd = await repo.GetAll();
+        dataAfterAdd.Should().HaveCount(4);
+        dataAfterAdd.Last().Should().BeSameAs(animeToAdd);
+        result.Should().BeSameAs(animeToAdd);
+
+    }
+    
+    [Fact]
+    public async Task Create_AddWithExistingId_ChangeIdAndAdd()
+    {
+        //Arrange
+        var contextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase("AnimeRepoTest")
+            .ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        await using var context = new AppDbContext(contextOptions);
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+
+        context.AddRange(_data);
+        await context.SaveChangesAsync();
+        
+        var repo = new AnimeRepository(context);
+
+        //Act
+        var animeThatExists = new Anime()
+        {
+            Id = 2, OriginalTitle = "かきくけこ", TranslatedTitle = "Title2", Status = Status.FinishedAiring,
+            Demographic = Demographic.Seinen, Description = "test desc2", ImgUrl = "https://google.com",
+            EndDate = new DateTime(2021, 4, 5), StartDate = new DateTime(2020, 12, 1), EpisodeCount = 10
+        };
+
+        var result = await repo.Create(animeThatExists);
+        await repo.SaveChangesAsync();
+
+        //Assert
+        var dataAfterAdd = await repo.GetAll();
+        dataAfterAdd.Should().HaveCount(4);
+        dataAfterAdd.Last().Should().NotBeSameAs(animeThatExists);
+        result.Should().NotBeSameAs(animeThatExists);
+
+    }
+}
