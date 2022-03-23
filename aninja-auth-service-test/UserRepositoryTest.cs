1ab921848bd240d09719d80038a2642ec5dfac98
rtrydev <r.kulka44@gmail.com>
2022-03-23 19:44:32 +0100

    add update test, login test

--- aninja-auth-service-test/UserRepositoryTest.cs
+++ aninja-auth-service-test/UserRepositoryTest.cs
@@ -76,6 +76,78 @@ namespace aninja_auth_service_test
             userFromRepo.Should().BeNull();
         }
 
+        [Fact]
+        public async Task GetUserByCredentials_Login_ReturnsUser()
+        {
+            //Arrange
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var repository = new UserRepository(mongoClient);
+
+            var guid = Guid.NewGuid();
+            var user = new User() { Id = guid, Name = "test", Email = "a@a.com", Password = "asd" };
+            await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").InsertOneAsync(user);
+
+            //Act
+            var userFromRepo = await repository.GetUserByCredentials(user.Name, user.Password);
+
+            //Assert
+            userFromRepo.Should().NotBeNull();
+            userFromRepo.Should().BeEquivalentTo(user);
+
+            //Teardown
+            await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").FindOneAndDeleteAsync(x => x.Id == guid);
+
+        }
+
+        [Fact]
+        public async Task GetUserByCredentials_LoginWrong_ReturnsNull()
+        {
+            //Arrange
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var repository = new UserRepository(mongoClient);
+
+            var guid = Guid.NewGuid();
+            var user = new User() { Id = guid, Name = "test", Email = "a@a.com", Password = "asd" };
+            await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").InsertOneAsync(user);
+
+            //Act
+            var userWrongPass = await repository.GetUserByCredentials(user.Name, "someotherpass");
+            var userWrongName = await repository.GetUserByCredentials("somename", user.Password);
+            var userBothWrong = await repository.GetUserByCredentials("somename", "somepass");
+
+            //Assert
+            userWrongPass.Should().BeNull();
+            userWrongName.Should().BeNull();
+            userBothWrong.Should().BeNull();
+
+            //Teardown
+            await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").FindOneAndDeleteAsync(x => x.Id == guid);
+        }
+
+        [Fact]
+        public async Task UpdateUser_ChangeEmail_UpdatesUser()
+        {
+            //Arrange
+            var mongoClient = new MongoClient("mongodb://localhost:27017");
+            var repository = new UserRepository(mongoClient);
+
+            var guid = Guid.NewGuid();
+            var user = new User() { Id = guid, Name = "test", Email = "a@a.com", Password = "asd" };
+            await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").InsertOneAsync(user);
+
+            //Act
+            user.Email = "b@b.com";
+            await repository.UpdateUser(user);
+            var userAfterUpdate = (await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").FindAsync(x => x.Id == guid)).First();
+
+            //Assert
+            userAfterUpdate.Should().NotBeNull();
+            userAfterUpdate.Should().BeEquivalentTo(user);
+
+            //Teardown
+            await mongoClient.GetDatabase("usersDB").GetCollection<User>("users").FindOneAndDeleteAsync(x => x.Id == guid);
+        }
+
 
     }
 }
