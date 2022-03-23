d642a66997aa859c550bb0790ff40444a4c86abd
rtrydev <r.kulka44@gmail.com>
2022-03-23 15:35:03 +0100

    add submodule, jwt test

---
+++ aninja-auth-service-test/JwtServiceTest.cs
@@ -0,0 +1,44 @@
+using Moq;
+using Xunit;
+using Microsoft.Extensions.Logging;
+using aninja_auth_service.Authorization;
+using aninja_auth_service.Models;
+using FluentAssertions;
+using System;
+using System.Threading;
+
+namespace aninja_auth_service_test
+{
+    public class JwtServiceTest
+    {
+        [Fact]
+        public async void GetToken_GetTokenByUser_ReturnsNewToken()
+        {
+            //Arrange
+            var loggerMock = new Mock<ILogger<JwtService>>();
+            loggerMock.Verify(logger => logger.Log(
+                It.Is<LogLevel>(logLevel => logLevel == LogLevel.Error),
+                It.Is<EventId>(eventId => eventId.Id == 0),
+                It.Is<It.IsAnyType>((@object, @type) => @object.ToString() == "myMessage" && @type.Name == "FormattedLogValues"),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                Times.Never
+            );
+            var jwtService = new JwtService(loggerMock.Object);
+
+            var someUser = new User() { Id = 1, Name = "Tester", Email = "a@a.com", Password = "pass123" };
+            Environment.SetEnvironmentVariable("JWT_SECRET", "someawesomejwtsecret");
+
+            //Act
+            var token = jwtService.GetJwtToken(someUser);
+            Thread.Sleep(1000);
+            var token2 = jwtService.GetJwtToken(someUser);
+
+            //Assert
+            token.Should().NotBeNull();
+            token2.Should().NotBeNull();
+            token.Should().NotBe(token2);
+            
+        }
+    }
+}
