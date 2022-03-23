c62443098b4f958efccc31a2ec4a9dd1db21e287
rtrydev <r.kulka44@gmail.com>
2022-03-23 16:04:06 +0100

    test logger in jwt

--- aninja-auth-service-test/JwtServiceTest.cs
+++ aninja-auth-service-test/JwtServiceTest.cs
@@ -6,13 +6,14 @@ using aninja_auth_service.Models;
 using FluentAssertions;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace aninja_auth_service_test
 {
     public class JwtServiceTest
     {
         [Fact]
-        public async void GetToken_GetTokenByUser_ReturnsNewToken()
+        public async Task GetToken_GetTokenByUser_ReturnsNewToken()
         {
             //Arrange
             var loggerMock = new Mock<ILogger<JwtService>>();
@@ -38,6 +39,37 @@ namespace aninja_auth_service_test
             token.Should().NotBeNull();
             token2.Should().NotBeNull();
             token.Should().NotBe(token2);
+
+            //Teardown
+            Environment.SetEnvironmentVariable("JWT_SECRET", null);
+
+        }
+
+        [Fact]
+        public async Task GetToken_SecretNotSet_CallsErrorLogReturnsNull()
+        {
+            //Arrange
+            var loggerMock = new Mock<ILogger<JwtService>>();
+            
+            var jwtService = new JwtService(loggerMock.Object);
+            var someUser = new User() { Id = 1, Name = "Tester", Email = "a@a.com", Password = "pass123" };
+
+            //Act
+            var token = jwtService.GetJwtToken(someUser);
+            Thread.Sleep(1000);
+            var token2 = jwtService.GetJwtToken(someUser);
+
+            //Assert
+            loggerMock.Verify(logger => logger.Log(
+                It.Is<LogLevel>(logLevel => logLevel == LogLevel.Error),
+                It.Is<EventId>(eventId => eventId.Id == 0),
+                It.Is<It.IsAnyType>((@object, @type) => @object.ToString() == "The JWT_SECRET environmental variable has not been set" && @type.Name == "FormattedLogValues"),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                Times.Exactly(2)
+            );
+            token.Should().BeNull();
+            token2.Should().BeNull();
             
         }
     }
